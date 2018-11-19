5802c22e0b604b4a87a859c662f81b45a75b41bb
aluno <emely-m2011@hotmail.com>
2018-11-19 09:08:43 -0200

    helpwe2

--- app/src/main/java/br/com/zottesso/persistenciaapp/ProdutosActivity.java
+++ app/src/main/java/br/com/zottesso/persistenciaapp/ProdutosActivity.java
@@ -2,38 +2,30 @@ package br.com.zottesso.persistenciaapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.List;
 
 public class ProdutosActivity extends AppCompatActivity {
 
-    private TextView textProdutos;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_produtos);
 
-        textProdutos = (TextView) findViewById(R.id.textProdutos);
+
 
         // Buscar todos os produtos do banco
         List<Produto> produtos = Produto.listAll(Produto.class);
 
-        // Guardar um texto com todos os produtos e valores
-        String texto = "";
+        ListView prodlis = (ListView)findViewById(R.id.lista);
 
-        // Percorrer a lista de produtos para montar o texto da tela
-        for(Produto p : produtos){
+        ArrayAdapter<Produto> ap = new ArrayAdapter<Produto>( context: this, android.R.layout.single_list_item_1, produtos);
+        prodlis.setAdapter(ap);
 
-            // Exemplo:
-            // Salgado - R$3.80 e \n para quebrar a linha do TextView
-            // texto = texto + "";
-            texto += p.getNome() + " - R$" + p.getValor() + "\n";
-
-        }
-
-        // Define o texto no textview
-        textProdutos.setText(texto);
     }
 }
